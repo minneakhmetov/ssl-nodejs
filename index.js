253ff01d707673e947548de5ffc19a9bab88aa0b
saiple <32297210+saiple@users.noreply.github.com>
2019-06-29 11:00:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express = require('express');
-const app = express();
+// Dependencies
 const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 
+const app = express();
 
 app.use(express.static('public'));
 
-app.listen(80);
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/spinachnow.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/spinachnow.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/spinachnow.com/chain.pem', 'utf8');
 
-console.log("Static NodeJS server started at 80");
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+app.use((req, res) => {
+    res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
